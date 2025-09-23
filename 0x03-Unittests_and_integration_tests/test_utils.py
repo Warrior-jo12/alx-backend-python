817ed5dd22014eab940d2331ce14df9e5b76f36a
Eyob <eyutialemu@gmail.com>
2025-09-23 11:54:03 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 import unittest
+import utils
 from parameterized import parameterized
 from utils import access_nested_map 
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,6 +29,26 @@ class TestAccessNestedMap(unittest.TestCase):
         # ensure the KeyError message matches the missing key
         self.assertEqual(str(context.exception), repr(path[-1]))
 
+ 
+class TestGetJson(unittest.TestCase):
+    """Unit tests for get_json"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        # Configure the mock to return a response with .json() method
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = utils.get_json(test_url)
+
+        # Assertions
+        mock_get.assert_called_once_with(test_url)  # check call with URL
+        self.assertEqual(result, test_payload)      # check returned data
 
 if __name__ == "__main__":
     unittest.main()
